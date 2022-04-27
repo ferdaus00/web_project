79415d302f8b948fc94ff30d21dfca712098bc69
ferdaus00 <104514753+ferdaus00@users.noreply.github.com>
2022-04-27 18:24:44 +0300

    Add files via upload

---
+++ OmaProject/kartta.js
@@ -0,0 +1,48 @@
+<html>
+<head>
+        <title> Helsingin maa- ja vesialueet</title>
+        <link href="t.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+    <div class="header">
+        <video autoplay loop class="tausta-video" muted plays-inline>
+                <source src="tausta.mp4" type="video/mp4">
+            </video>
+
+
+        <nav>
+            <img src="logo1.jpg" class="logo">
+            <ul class="nav-links">
+                <li><a href="maa.html"> Maa ja Vesialueet </a></li>
+                <li><a href="kartta.html"> Kartta </a></li>
+                <li><a href="inffo.html">Inffo </a></li>
+                <li class="btn">Lähetä viesti</a></li>
+            </ul>
+        </nav> 
+
+        <div class="content">
+            <h1>Helsingin maa- ja vesialueet</h1>
+            <form>
+                <input type="text" placeholder="&#x270e;Etsi alue postinumerolla">
+                <button type="submit">Etsi </button>
+            </form> 
+
+            <div class="category-list">
+                 <div class="category">
+                    <img src="map.png">
+                    <p> Kartta 
+
+
+
+
+
+
+
+
+        </div>
+    </div>
+
+
+</body>
+<html></html>
